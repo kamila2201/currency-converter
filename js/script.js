0ca8288c151eed7909e6f857b5d8a18b29ffa55f
Kamila Maciąg <kamila.maciag22@gmail.com>
2023-02-03 19:34:56 +0100

    functions have been closed in a block

--- js/script.js
+++ js/script.js
@@ -1,41 +1,43 @@
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let formElement = document.querySelector(".js-form");
-let resultElement = document.querySelector(".js-result");
+{
+    let amountElement = document.querySelector(".js-amount");
+    let currencyElement = document.querySelector(".js-currency");
+    let formElement = document.querySelector(".js-form");
+    let resultElement = document.querySelector(".js-result");
 
-let rateEUR = 4.67;
-let rateUSD = 4.39;
-let rateCHF = 4.72;
-let rateGBP = 5.3;
+    let rateEUR = 4.67;
+    let rateUSD = 4.39;
+    let rateCHF = 4.72;
+    let rateGBP = 5.3;
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    let amount = +amountElement.value;
-	let currency = currencyElement.value;
+        let amount = +amountElement.value;
+        let currency = currencyElement.value;
 
-    let result;
+        let result;
 
-    switch (currency) {
-        case "EUR":
-            result = amount / rateEUR;
-            break;
-        case "USD":
-            result = amount / rateUSD;
-            break;
-        case "CHF":
-            result = amount / rateCHF;
-            break;
-        case "GBP":
-            result = amount / rateGBP;
-            break;
-    }
+        switch (currency) {
+            case "EUR":
+                result = amount / rateEUR;
+                break;
+            case "USD":
+                result = amount / rateUSD;
+                break;
+            case "CHF":
+                result = amount / rateCHF;
+                break;
+            case "GBP":
+                result = amount / rateGBP;
+                break;
+        }
 
 
 
-    resultElement.innerText = (`${result.toFixed(2)} ${currency}`);
+        resultElement.innerText = (`${result.toFixed(2)} ${currency}`);
 
-});
+    });
+}
 
 
 
